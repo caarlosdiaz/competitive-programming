c1b42dc260dfedef76f2435e3f391958ef7cd344
Carlos Díaz Abad <cadiabcreations@gmail.com>
2025-02-08 17:44:42 +0100

    feat: implemented erdos numbers in python

--- chap-2/erdos/erdos_c.py
+++ chap-2/erdos/erdos_c.py
@@ -1,7 +1,9 @@
-import sys
-from collections import deque
+from collections import deque, defaultdict
 
-input = sys.stdin.read
+# Key -> Author name
+# Value -> Authors connected (list)
+authorsGraph = defaultdict(set)
+authorsWeight = {}
 
 def join_authors(authors, length: int) -> list:
     i = 0
@@ -11,55 +13,86 @@ def join_authors(authors, length: int) -> list:
         authorCompleteName = authors[i] + ', ' + authors[i + 1]
         res.append(authorCompleteName)
         i += 2
+    
+    return res
 
-def process_author_name(papers: list):
-    for paper in papers:
-        authorsRaw = paper.split(": ")[0]
-        authors = authorsRaw.split(", ")
-        res = []
-        length = len(authors)
+def process_author_name(paper: str):
+    authorsRaw = paper.split(": ")[0]
+    authors = authorsRaw.split(", ")
+    res = []
+    length = len(authors)
+    
 
-        if length % 2 == 0:
-            res = join_authors(authors, length)
+    if length % 2 == 0:
+        res = join_authors(authors, length)
 
-        else:
-            last = authors.pop()
-            res = join_authors(authors)
-            res.append(last)
+    else:
+        length -= 1
+        last = authors.pop()
+        res = join_authors(authors, length)
+        res.append(last)
         
     return res
 
-def calculate_erdos_number(papers: list, autores_consultados):
-    # Key -> Author name
-    # Value -> Authors connected (list)
-    graph = {}
-    for paper in papers:
-        autores = process_author_name(paper, graph)
+def calculate_erdos_number():
+    initial_node = "Erdos, P."
+    if initial_node not in authorsGraph:
+        print("Erdos not in graph")
+        return
+
+    queue = deque()
+    queue.append(initial_node)
+    authorsWeight[initial_node] = 0
+
+    while queue:
+        node = queue.popleft()
+
+        navigableNodes = authorsGraph[node]
+        
+        for nd in navigableNodes:
+            if nd in authorsWeight:
+                continue
+            queue.append(nd)
+            authorsWeight[nd] = authorsWeight[node] + 1
+
+def get_erdos_number(author):
+    if author not in authorsGraph:
+        print(author + " infinity")
+        return
     
+    if author in authorsWeight:
+        print(author + " " + str(authorsWeight[author]))
+        return
+    
+    print(author + " infinity")
 
 def main():
     casos = int(input())
     for caso in range(casos):
-        if caso > 0:
-            print()
+        authorsGraph.clear()
+        authorsWeight.clear()
    
-    papers = []
-    autores = []
-    
-    P, N = map(int, input().split())
-    for _ in range(P):
-        papers.append(input())
-    
-    for _ in range(N):
-        autores.append(input())
+        papers = []
+        authorsToCalc = []
         
+        P, N = map(int, input().split())
+        for _ in range(P):
+            papers.append(input())
         
-    print(f"Scenario {caso + 1}")
-    res = calculate_erdos_number(papers, autores_consultados)
-        
-    
-       
-   
+        for _ in range(N):
+            authorsToCalc.append(input())
+
+        for paper in papers:
+            authors = process_author_name(paper)
+            for a in authors:
+                authorsGraph[a].update(set(authors) - {a})
+
+        calculate_erdos_number()
+
+        print("Scenario " + str(caso + 1))
+
+        for author in authorsToCalc:
+            get_erdos_number(author)
 
 if __name__ == "__main__":
 	main()
