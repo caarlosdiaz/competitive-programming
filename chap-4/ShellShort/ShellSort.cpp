888cb101aa7bf5bd791d8c345b6f4ba6d0d6c31b
Carlos Díaz Abad <cadiabcreations@gmail.com>
2025-02-17 13:58:34 +0100

    feat: implemented shell sort

--- chap-4/ShellShort/ShellSort.cpp
+++ chap-4/ShellShort/ShellSort.cpp
@@ -1,62 +1,47 @@
 #include <iostream>
 #include <vector>
-
+#include <string>
+#include <unordered_map>
 using namespace std;
 
-int main(int argc, char *argv[])
-{
+int main() {
     int cases;
     cin >> cases;
     cin.ignore();
-
-    while (cases--)
-    {
-        string originalNames[200];
-        int originalPosition[200]; // being i the target pos, originalPosition[i] is the original pos
-
+    
+    while(cases--) {
         int turtlesNumber;
         cin >> turtlesNumber;
         cin.ignore();
-
-        string buffer;
-
-        for (int i = 0; i < turtlesNumber; i++)
-        {
-            getline(cin, buffer);
-            originalNames[i] = buffer;
+        
+        vector<string> originalNames(turtlesNumber);
+        vector<string> expectedNames(turtlesNumber);
+        unordered_map<string, int> positionMap;
+        
+        for (int i = 0; i < turtlesNumber; i++) {
+            getline(cin, originalNames[i]);
+            positionMap[originalNames[i]] = i;
         }
         
-        for (int i = 0; i < turtlesNumber; i++)
-        {
-            getline(cin, buffer);
-            for (int j = 0; j < turtlesNumber; j++)
-            {
-                if (originalNames[j] == buffer)
-                {
-                    originalPosition[i] = j;
-                    break;
-                }
-            }
+        for (int i = 0; i < turtlesNumber; i++) {
+            getline(cin, expectedNames[i]);
         }
-
-        int i;
         
-        for (i = turtlesNumber - 1; i >= 0; i--)
-        {
-            if (i == 0 || originalPosition[i] < originalPosition[i - 1])
-            {
-                i--;
-                break;
-            }
+        vector<int> originalPosition(turtlesNumber);
+        for (int i = 0; i < turtlesNumber; i++) {
+            originalPosition[i] = positionMap[expectedNames[i]];
         }
-
-        while (i >= 0)
-        {
-            int ogPos = originalPosition[i];
-            cout << originalNames[ogPos] << endl;
-            i--;
+        
+        int j = turtlesNumber - 1;
+        while(j > 0 && originalPosition[j - 1] < originalPosition[j])
+            j--;
+        
+        for (int i = j - 1; i >= 0; i--) {
+            cout << expectedNames[i] << endl;
         }
-
-        if (cases > 0) cout << endl;
+        
+        cout << endl;
     }
+    
+    return 0;
 }
