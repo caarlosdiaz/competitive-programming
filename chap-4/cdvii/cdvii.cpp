9671e73fa65997482f3d10e8a5587fa4c9412db8
Carlos Díaz Abad <cadiabcreations@gmail.com>
2025-02-28 13:24:00 +0100

    feat: completed cdvii

--- chap-4/cdvii/cdvii.cpp
+++ chap-4/cdvii/cdvii.cpp
@@ -3,6 +3,8 @@
 #include <sstream>
 #include <vector>
 #include <algorithm>
+#include <map>
+#include <cstdlib>
 
 using namespace std;
 
@@ -22,10 +24,7 @@ public:
 
         for (int i = 0; i < 4; i++)
         {
-            if (!getline(timeStream, s, ':'))
-            {
-                throw invalid_argument("Formato de tiempo incorrecto: " + timeString);
-            }
+            getline(timeStream, s, ':');
 
             if (i == 0)
                 continue;
@@ -81,6 +80,7 @@ private:
     int location;
 
 public:
+    Record() : plate(""), time(Time()), io(""), location(0) {}
     Record(string line)
     {
         istringstream lineStream(line);
@@ -128,6 +128,11 @@ bool comparer(Record r1, Record r2)
     return r2.isBiggerThan(r1);
 }
 
+double calculateFee(int hourRates[24], int entryHour, int in, int out)
+{
+    return (hourRates[entryHour] * abs(in - out)) / 100.00;
+}
+
 int main(int argc, char *argv[])
 {
     int cases;
@@ -137,7 +142,11 @@ int main(int argc, char *argv[])
     {
         int hourRates[24];
         vector<Record> records;
+        map<string, double> prices;
+        map<string, Record> tempRecs;
+        vector<string> processedPlates;
         records.reserve(1000);
+
 
         for (int i = 0; i < 24; i++)
         {
@@ -146,7 +155,7 @@ int main(int argc, char *argv[])
         cin.ignore();
 
         string s;
-        while (getline(cin, s) && s != "\n")
+        while (getline(cin, s) && !s.empty())
         {
             records.push_back(Record(s));
         }
@@ -155,8 +164,39 @@ int main(int argc, char *argv[])
 
         for (int i = 0; i < records.size(); i++)
         {
-            cout << records[i].getPlate() << " " << records[i].getTime().getDay() << ":" << records[i].getTime().getHour() << ":" << records[i].getTime().getMin() << endl;
+            string plate = records[i].getPlate();
+            if (tempRecs.count(plate) && records[i].getIo() == "exit")
+            {
+                if (!prices.count(plate)){
+                    prices[plate] = 2;
+                    processedPlates.push_back(plate);
+                }
+
+                prices[plate] += 1 + calculateFee(
+                                        hourRates,
+                                        tempRecs[plate].getTime().getHour(),
+                                        tempRecs[plate].getLocation(),
+                                        records[i].getLocation());
+
+                tempRecs.erase(plate);
+                continue;
+            }
+
+            if (records[i].getIo() == "enter") tempRecs[plate] = records[i];
+
         }
+
+        sort(processedPlates.begin(), processedPlates.end());
+
+        for (int i = 0; i < processedPlates.size(); i++)
+        {
+            cout.setf(ios::fixed);
+            cout.setf(ios::showpoint);
+            cout.precision(2);
+            cout << processedPlates[i] << " $" << prices[processedPlates[i]] << endl;
+        }
+        
+        if (cases > 0) cout << endl;
     }
 
     return 0;
